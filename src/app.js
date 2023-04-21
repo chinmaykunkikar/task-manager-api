6ec15919c0bbf974e58da596d906e58ff554ed17
Chinmay Kunkikar <chinmay.kunkikar@gmail.com>
2023-04-21 20:43:23 +0530

    Write new task to json file

--- src/app.js
+++ src/app.js
@@ -1,6 +1,8 @@
 const Ajv = require("ajv");
-const express = require("express");
 const bodyParser = require("body-parser");
+const express = require("express");
+const fs = require("fs");
+const path = require("path");
 const tasks = require("./tasks.json");
 
 const app = express();
@@ -12,7 +14,7 @@ const ajv = new Ajv();
 const taskSchema = {
   type: "object",
   properties: {
-    id: { type: "string", minLength: 1 },
+    id: { type: "number", minLength: 1 },
     title: { type: "string", minLength: 1 },
     description: { type: "string", minLength: 1 },
     completed: { type: "boolean" },
@@ -29,9 +31,19 @@ app.get("/tasks", (req, res) => {
 // POST create a new task
 app.post("/tasks", (req, res) => {
   const newTask = req.body;
+  let writePath = path.join(__dirname, "..", "tasks.json");
+  let tasksModified = JSON.parse(JSON.stringify(tasks));
   const validBody = ajv.validate(taskSchema, newTask);
-  if (validBody) res.status(201).json(newTask);
-  res.status(400).json({ message: "Task data is not valid" });
+  if (validBody) {
+    tasksModified.tasks.push(newTask);
+    fs.writeFileSync(writePath, JSON.stringify(tasksModified), {
+      encoding: "utf8",
+      flag: "w",
+    });
+    res.status(201).json(newTask);
+  } else {
+    res.status(400).json({ message: "Invalid task data" });
+  }
 });
 
 app.listen(3000, () => {
